bdfdd0ea9a55382dcec018495bd8ff40f02e14ea
ADAIBLOG <186315423+ADAIBLOG@users.noreply.github.com>
2026-01-29 18:47:00 +0800

    Update messages_all.dart

--- lib/generated/intl/messages_all.dart
+++ lib/generated/intl/messages_all.dart
@@ -19,12 +19,14 @@ import 'package:intl/src/intl_helpers.dart';
 import 'messages_de.dart' as messages_de;
 import 'messages_en.dart' as messages_en;
 import 'messages_tr.dart' as messages_tr;
+import 'messages_zh.dart' as messages_zh;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
   'de': () => new SynchronousFuture(null),
   'en': () => new SynchronousFuture(null),
   'tr': () => new SynchronousFuture(null),
+  'zh': () => new SynchronousFuture(null),
 };
 
 MessageLookupByLibrary? _findExact(String localeName) {
@@ -35,6 +37,8 @@ MessageLookupByLibrary? _findExact(String localeName) {
       return messages_en.messages;
     case 'tr':
       return messages_tr.messages;
+    case 'zh':
+      return messages_zh.messages;
     default:
       return null;
   }
